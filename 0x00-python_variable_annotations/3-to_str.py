7e7671b66de1ff5281ba484aefafd03d13d36e21
hafdhisofien <1114@holbertonschool.com>
2021-03-15 20:30:05 +0100

    str rep

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+str representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+        n: given float
+    Returns:
+        str reprsentation of n
+    """
+    return n.__str__()
