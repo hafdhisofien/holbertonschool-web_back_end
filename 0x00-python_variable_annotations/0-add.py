5812eba3c974f7193bd43718bf2419c87130edc3
hafdhisofien <1114@holbertonschool.com>
2021-03-15 20:12:15 +0100

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Add two floats
+"""
+
+def add(a:float, b:float)-> float:
+    """
+    Args:
+        a: float number
+        b: float number
+    Returns:
+        sum as a float
+    """
+    return a+b
