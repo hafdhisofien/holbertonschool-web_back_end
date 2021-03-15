40db4978d7eb867ce477f3d31ea207a6ff015d37
hafdhisofien <1114@holbertonschool.com>
2021-03-15 20:24:40 +0100

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+floor a given float
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Args:
+        n: float to be floord
+    Returns:
+        floord float
+    """
+    return math.floor(n)
