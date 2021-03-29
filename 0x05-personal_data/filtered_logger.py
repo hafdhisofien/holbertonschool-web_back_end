623429583f7ca670aea72b6df1222c468346457d
hafdhisofien <1114@holbertonschool.com>
2021-03-29 15:51:47 +0100

    regex

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+0. Regex-ing
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """
+    Args:
+        fields: a list of strings representing all fields to obfuscate
+        redaction: a string representing by what the field will be obfuscated
+        separator: a string representing by which
+        character is separating all fields in the log line
+    Returns:
+        message: a string representing the log line
+    """
+    for field in fields:
+        message = re.sub(field + "=.+?(?=abc)*\\" + ";", field + "=" + redaction + separator, message)
+    return message
