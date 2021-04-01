c1f062be8bc4d963227c9facfa2c5675e92fea4e
hafdhisofien <1114@holbertonschool.com>
2021-04-01 17:51:02 +0100

    paths

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -14,6 +14,14 @@ class Auth:
         """
         require authentication
         """
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        else:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
