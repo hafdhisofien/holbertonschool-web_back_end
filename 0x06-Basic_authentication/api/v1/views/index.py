bc5da9462a5074751ee3dd463e40fde5c3395f62
hafdhisofien <1114@holbertonschool.com>
2021-04-01 17:32:02 +0100

    pep8

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -13,13 +13,15 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
-    Return: 
+    Return:
       - Status unauthorized 401
     """
     abort(401)
+
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
