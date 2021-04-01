8dac9c1e99cf249a2c831ba2342d265b2772ccab
hafdhisofien <1114@holbertonschool.com>
2021-04-01 19:02:11 +0100

    email & pwd

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,3 +40,18 @@ class BasicAuth(Auth):
             return base64.b64decode(decoded).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """
+        returns the user email and password from the Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        em_pwd = decoded_base64_authorization_header.split(':', 1)
+        return em_pwd[0], em_pwd[1]
