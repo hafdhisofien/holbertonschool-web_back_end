f7c18d8b7145ea09416978815754a25aeecf2386
hafdhisofien <1114@holbertonschool.com>
2021-04-22 23:32:57 +0100

    urls

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+1. Basic Babel setup
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    class config
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def welcome_holberton() -> str:
+    """
+    render of index
+    """
+    return render_template('0-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Get best supported language
+    """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config.get['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
