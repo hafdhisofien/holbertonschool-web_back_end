412d5231ca5eaaffd5c510a6e94640c4a0aa27fe
hafdhisofien <1114@holbertonschool.com>
2021-05-11 23:28:39 +0100

    await

---
+++ 0x0F-ES6_promise/100-await.js
@@ -0,0 +1,20 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let photo;
+  let user;
+
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+    return {
+      photo,
+      user,
+    };
+  } catch (e) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
