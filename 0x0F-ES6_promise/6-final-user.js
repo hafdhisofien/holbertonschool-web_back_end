7abcf5fbe4bd55a9a528d1933b5ef2aaa7254444
hafdhisofien <1114@holbertonschool.com>
2021-05-11 23:16:50 +0100

    manda

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,6 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+}
