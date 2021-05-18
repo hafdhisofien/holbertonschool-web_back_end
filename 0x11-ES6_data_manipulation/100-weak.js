3e56f473ca12eed55f7c1867f9fe6fb1b942e3ef
hafdhisofien <1114@holbertonschool.com>
2021-05-18 20:25:49 +0100

    lint

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,8 @@
 export const weakMap = new WeakMap();
-
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
-  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
   weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
 }
