f6af6f8bf6f68ad02d949afe48480de8bbb03894
hafdhisofien <1114@holbertonschool.com>
2021-05-26 19:25:16 +0100

    include

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,4 +1,4 @@
-const calculateNumber = require('./1-calcul.js');
+const calculateNumber = require('./2-calcul_chai.js');
 const chai = require('chai');
 
 describe("smoke test for type of SUM", function() {
